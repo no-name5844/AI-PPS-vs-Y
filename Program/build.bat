cc8ac07b1ee6280aa08731acce8f14bf682a840f
a111 <a111@163.com>
2026-05-04 14:05:07 +0800

    docs: 添加程序文档和测试结果记录
    添加.gitignore文件忽略.trae目录
    新增Program目录下的可执行文件和构建脚本
    更新README.md记录程序发现和分析过程
    创建MEMORY.md和ANALYSIS_TABLE.md记录测试结果和对比分析

---
+++ Program/build.bat
@@ -0,0 +1,3 @@
+cd "G:\4\ord\expand\PPS && 1-Y\AutoGuogaoMachine\test\
+g++ -std=c++17 -I.. ..\YSequence.cpp YSequence.cpp -o y-expand.exe
+g++ -std=c++17 -I.. ..\PPSNotation.cpp PPSNotation.cpp -o pps-notation.exe
